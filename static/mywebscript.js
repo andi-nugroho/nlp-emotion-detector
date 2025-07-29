2984a0c72b5bf1797b0d12e909ef3dc5676bdf3f
andi-nugroho <102736954+andi-nugroho@users.noreply.github.com>
2025-07-29 18:34:15 +0700

    Final Project: Complete emotion detection application

--- static/mywebscript.js
+++ static/mywebscript.js
@@ -1,5 +1,5 @@
-let RunSentimentAnalysis = ()=>{
-    textToAnalyze = document.getElementById("textToAnalyze").value;
+function RunSentimentAnalysis() {
+    let textToAnalyze = document.getElementById("textToAnalyze").value;
 
     let xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
@@ -10,3 +10,4 @@ let RunSentimentAnalysis = ()=>{
     xhttp.open("GET", "emotionDetector?textToAnalyze"+"="+textToAnalyze, true);
     xhttp.send();
 }
+
